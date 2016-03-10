5b7ff20cad5ba49dbd8d135f9509b4d06c91c9fe
Vladimir Vladkov <djuoen9505@gmail.com>
2016-03-10 17:18:16 +0100

    test

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Hallo</title>
+</head>
+<body>
+<h1 align="center">Hallo</h1>
+    <form action="process.php" method="post">
+        Namen eingeben<input name="Name" type="text">
+        <input type="submit">
+    </form>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+</head>
+<body>
+Hallo <?php echo $_POST['Name']; ?>
+</body>
+</html>
